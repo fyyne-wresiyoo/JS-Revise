708e9df791143bd3e3fa22fa17a6fec7571510bd
new divide <bistpramod113@gmail.com>
2026-01-29 15:00:24 +0545

    Add operators.js file

---
+++ variables and data types/operators.js
@@ -0,0 +1,40 @@
+// operators are the symbols which perform any kind of operation 
+// arithmetic operations , comparison operations , logical operations etc
+// ARITHMETIC OPERATORS
+let x = 10;
+let y = 4;
+
+console.log(x + y); // Addition
+console.log(x - y); // Subtraction
+console.log(x * y); // Multiplication
+console.log(x / y); // Division
+console.log(x % y); // Modulus
+console.log(x ** y); // Exponentiation  
+
+
+
+// ASSIGNMENT OPERATORS 
+let a = 10; // Assignment
+a += 5; // a = a + 5  its the same
+
+console.log(a); // 15
+
+
+
+//COMPARISON OPERATORS 
+
+let p = 10;
+let q = '10';
+
+console.log(p==q);
+
+console.log(p===q);// Greater than
+
+
+//logical operators 
+let bool1 = true;
+let bool2 = false;                      
+console.log(bool1 && bool2); // AND
+console.log(bool1 || bool2);    // OR
+console.log(!bool1); // NOT // NOT      
+// NOT operator is used to reverse the logical state of its operand. If a condition is true, NOT operator will make it false and vice versa.
