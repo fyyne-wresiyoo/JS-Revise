1c797cc568b29a80a09102cf453d8b89cd0a2e76
Pramod Bist <155552124+bistpramod@users.noreply.github.com>
2026-02-01 19:30:25 +0545

    Add files via upload

--- variables and data types/operators.js
+++ variables and data types/operators.js
@@ -28,7 +28,7 @@ let q = '10';
 
 console.log(p==q);
 
-console.log(p===q);// Greater than
+console.log(p===q);// Strict Equality
 
 
 //logical operators 
@@ -36,5 +36,37 @@ let bool1 = true;
 let bool2 = false;                      
 console.log(bool1 && bool2); // AND
 console.log(bool1 || bool2);    // OR
-console.log(!bool1); // NOT // NOT      
+console.log(!bool1); // NOT
 // NOT operator is used to reverse the logical state of its operand. If a condition is true, NOT operator will make it false and vice versa.
+
+
+
+
+//ternary operator
+console.log("Ternary Operator Example:");
+let age = 18;
+let canVote = (age >= 18) ?  "Yes, can vote" : "No, cannot vote";
+console.log(canVote); 
+/** 
+ * let variable = value
+ * let anotherVariable = condition ? valueIfTrue : valueIfFalse
+ * console.log(variable);
+ */
+
+
+
+
+// uninary operators
+
+// post and pre
+console.log("Unary Operator Example:");
+let count = 5;
+
+console.log(count++); // Post-increment: returns 5, then count becomes 6
+console.log(count); // Presents 6
+console.log(++count); // Pre-increment: count becomes 7, then returns 7
+
+console.log(count--); // Post-decrement: returns 7, then count becomes 6
+console.log(--count); // Pre-decrement: count becomes 5, then returns 5
+
+
