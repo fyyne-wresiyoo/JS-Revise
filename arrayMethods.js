93707009cae7635bbe4db0b063617971460ca5a4
prWhana <155552124+fyyne-wresiyoo@users.noreply.github.com>
2026-02-06 21:29:48 +0545

    Add files via upload

---
+++ arrayMethods.js
@@ -0,0 +1,26 @@
+// some methods should be remembered in arrays
+
+// higher order methods
+
+const numbers= [1,2,3,4,6,7,8,]
+
+
+// looping array
+// for each 
+// arr.forEach(callback)
+
+
+const callBack = () => {
+
+}
+
+numbers.forEach(callBack)
+
+
+// filete 
+//arr.filter(callback)
+// returns new array
+
+
+
+
